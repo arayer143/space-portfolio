9279fdedc04080fbe3f8314c096fe8190b503f8e
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-04-23 21:24:24 -0500

    removed video from hero section

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -13,6 +13,7 @@ const Projects = () => {
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
           src="/cleanslatelogo.png"
+          
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
