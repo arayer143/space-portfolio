bc8b5ca3765582922ede8e9e2f75168b6c768d72
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-04-23 23:59:11 -0500

    removed the encryption section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Encryption from "@/components/main/Encryption";
+
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
@@ -10,7 +10,6 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
-        <Encryption />
         <Projects />
       </div>
     </main>
