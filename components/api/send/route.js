e2c9a43a1e9ea28238dbf39d5684621dc95c3544
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-04-30 13:39:06 -0500

    change to the js file again

--- components/api/send/route.js
+++ components/api/send/route.js
@@ -1,26 +1,30 @@
-
+import { NextResponse } from 'next/server'
 import { Resend } from "resend";
 
 const resend = new Resend(re_CKJ8GYqw_mTnU5dBoSTtkRTTcTAsNxSDD);
 
-await resend.emails.send({
-  from: 'Acme <onboarding@resend.dev>',
-  to: ['delivered@resend.dev'],
-  subject: 'hello world',
-  text: 'it works!',
-  attachments: [
-    {
-      filename: 'invoice.pdf',
-      content: invoiceBuffer,
-    },
-  ],
-  headers: {
-    'X-Entity-Ref-ID': '123456789',
-  },
-  tags: [
-    {
-      name: 'category',
-      value: 'confirm_email',
-    },
-  ],
-});
+resend.apiKeys.list();
+
+
+export async function POST(req, _res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: {email},
+      to: 'alexrayer7@gmail.com',
+      subject: 'Hello World',
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
