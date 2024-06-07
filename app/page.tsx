931c82f893987411350fbec2fbd90b5bd31002e2
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-06-07 15:04:36 -0500

    added the contact section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
+import EmailSection from "@/components/main/EmailSection";
+
 
 
 
@@ -15,6 +17,7 @@ export default function Home() {
         <Hero />
         <Skills />
         <Projects />
+        <EmailSection />
       
       </div>
     </main>
